83cdb7109ec233c2fcab98ee9f110ad545edac23
Kamal Max <pmer93@gmail.com>
2017-12-07 00:07:43 +0330

    R project for word frequency added

---
+++ probability/text_frequency/word_freq.R
@@ -0,0 +1,25 @@
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+
+tf = function(fileAdd) {
+  text = readLines(fileAdd)
+
+  corpusText <- Corpus(VectorSource(text))
+  corpusText_data <- tm_map(corpusText, stripWhitespace)
+  corpusText_data <- tm_map(corpusText_data, tolower)
+  corpusText_data <- tm_map(corpusText_data, removeNumbers)
+  corpusText_data <- tm_map(corpusText_data, removePunctuation)
+  corpusText_data <- tm_map(corpusText_data, removeWords, stopwords("english"))
+  corpusText_data <- tm_map(corpusText_data, removeWords,
+    c("one","and","the","our","that","for","are","also","more","has","must","have","should","this","with", "will", "unto"))
+
+  tdmText <- TermDocumentMatrix(corpusText_data)
+  TDM1 <- as.matrix(tdmText)
+  v = sort(rowSums(TDM1), decreasing = TRUE)
+
+  wordcloud(corpusText_data, scale=c(5,0.5), max.words=20, random.order=FALSE, rot.per=0.35, use.r.layout=FALSE, colors=brewer.pal(8, "Dark2"))
+}
+
+tf("text")
+tf("text2")
