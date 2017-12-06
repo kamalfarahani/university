8270dc622d240b26877bcb029c69f7a4abad5693
Kamal Max <pmer93@gmail.com>
2017-12-07 00:20:19 +0330

    more remove words added

--- probability/text_frequency/word_freq.R
+++ probability/text_frequency/word_freq.R
@@ -12,7 +12,7 @@ tf = function(fileAdd) {
   corpusText_data <- tm_map(corpusText_data, removePunctuation)
   corpusText_data <- tm_map(corpusText_data, removeWords, stopwords("english"))
   corpusText_data <- tm_map(corpusText_data, removeWords,
-    c("one","and","the","our","that","for","are","also","more","has","must","have","should","this","with", "will", "unto"))
+    c("thus","one","and","the","our","that","for","are","also","more","has","must","have","should","this","with", "will", "unto"))
 
   tdmText <- TermDocumentMatrix(corpusText_data)
   TDM1 <- as.matrix(tdmText)
